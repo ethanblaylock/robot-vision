dcdc1791732a3642981458bca798f1bc263a324e
Ethan Blaylock <ethan.blaylock37@gmail.com>
2024-06-08 06:23:44 +0000

    was able to mesh gazebo and ros together and bridge the message between them

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     )
  
     # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = ExecuteProcess(cmd=['ign','gazebo','empty.sdf'])
+    gazebo = ExecuteProcess(cmd=['gz','sim','empty.sdf'])
              
  
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -36,11 +36,30 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
  
+    message_transmit1 = Node(package='ros_gz_bridge', executable='parameter_bridge',
+                        arguments=['/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'])
+
+    message_transmit2 = Node(package='ros_gz_bridge', executable='parameter_bridge',
+                        arguments=['/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V'])
+
+
+    message_transmit3 = Node(package='ros_gz_bridge', executable='parameter_bridge',
+                        arguments=['/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model'])
+
+    message_transmit4 = Node(package='ros_gz_bridge', executable='parameter_bridge',
+                        arguments=['/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry'])
+
  
- 
+
+    #ros2 run ros_gz_bridge parameter_bridge /cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist
+
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        message_transmit1,
+        message_transmit2,
+        message_transmit3,
+        message_transmit4,
     ])
